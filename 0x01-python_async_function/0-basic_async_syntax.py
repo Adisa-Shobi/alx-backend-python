ef2d104652acc0cc143eef7549aa9d687878fb74
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-02-06 16:41:07 +0000

    Create: '0-basic_async_syntax.py' asynchronous coroutine

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+'''
+Asynchronous coroutine waits for a random delay between 0 and max_delay
+'''
+import random
+import time
+
+
+async def wait_random(max_delay: int = 10):
+    '''
+    Max delay function
+    '''
+    random_duration = random.uniform(0, max_delay)
+    time.sleep(random_duration)
+    return random_duration
