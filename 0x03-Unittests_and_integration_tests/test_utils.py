e41befa77a6fa93268911ba6ba87f5b3915f0190
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-02-26 10:41:19 +0000

    Update: 'test_utils.py' test with mock patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,9 +3,10 @@
 TestAccessNestedMap class that inherits from unittest.TestCase
 '''
 import unittest
+from unittest.mock import Mock
 from parameterized import parameterized
-from typing import Mapping, Sequence, Union
-from utils import access_nested_map
+from typing import Mapping, Sequence, Union, Any
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -27,3 +28,38 @@ class TestAccessNestedMap(unittest.TestCase):
         Test access nested map
         '''
         self.assertEqual(access_nested_map(nested_map, path), result)
+
+    @parameterized.expand([
+        ({}, ["a"]),
+        ({"a": 1}, ["a", "b"])
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Mapping,
+            path: Sequence):
+        '''
+        tests for errors raised by access_nested_map
+        '''
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    '''
+    Tests the utils.get_json func
+    '''
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @unittest.mock.patch('requests.get')
+    def test_get_json(self, test_url: str, test_payload: Mapping, mock_get):
+        '''
+        tests get_json method with different urls
+        '''
+        my_mock = Mock()
+        my_mock.json.return_value = test_payload
+        mock_get.return_value = my_mock
+        self.assertEqual(get_json(test_url), test_payload)
+        mock_get.assert_called_once_with(test_url)
