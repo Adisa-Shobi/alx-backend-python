c29e1aad24955255d26d35a67f4fbadffd0a2709
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-02-26 16:09:52 +0000

    Update: 'test_client.py' testing properties

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 Testing client file methods
 '''
 import unittest
-from unittest.mock import Mock
+from unittest.mock import Mock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -27,3 +27,15 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get.assert_called_once_with(
             "https://api.github.com/orgs/{org}".format(org=org)
         )
+
+    @unittest.mock.patch(
+        'client.GithubOrgClient.org',
+        new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        '''
+        unit-test GithubOrgClient._public_repos_url
+        '''
+        repo_url = GithubOrgClient("google")
+        mock_resp = {"repos_url": "https://api.github.com/orgs/"}
+        mock_org.return_value = mock_resp
+        self.assertEqual(repo_url._public_repos_url, mock_resp['repos_url'])
