ea7dcf331ba0e231129541d2838933005f0fbebc
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-02-26 13:19:59 +0000

    Update: 'test_client.py' new test case added

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,7 @@ import unittest
 from unittest.mock import Mock
 from parameterized import parameterized
 from typing import Mapping, Sequence, Union, Any
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -63,3 +63,28 @@ class TestGetJson(unittest.TestCase):
         mock_get.return_value = my_mock
         self.assertEqual(get_json(test_url), test_payload)
         mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    '''
+    tests utils.memoize decorator
+    '''
+
+    def test_memoize(self):
+        '''
+        test memoization decorator
+        '''
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with unittest.mock.patch.object(TestClass, 'a_method') as mock_mthd:
+            test = TestClass()
+            test.a_property()
+            test.a_property()
+            mock_mthd.assert_called_once()
