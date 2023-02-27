f5605f7a86e2a00eba081e39828c4657a9105a64
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-02-27 05:38:18 +0000

    Update: '([{name: google}, {name: microsoft}, {name: excel}],'

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,7 +5,7 @@ Testing client file methods
 import unittest
 from unittest.mock import Mock, PropertyMock
 from parameterized import parameterized
-from client import GithubOrgClient
+from client import GithubOrgClient, get_json
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -39,3 +39,26 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_resp = {"repos_url": "https://api.github.com/orgs/"}
         mock_org.return_value = mock_resp
         self.assertEqual(repo_url._public_repos_url, mock_resp['repos_url'])
+
+    @parameterized.expand([
+        ([{"name": "google"}, {"name": "microsoft"}, {"name": "excel"}],
+         "https://api.github.com/orgs/test"),
+        ([{"name": "axios"}, {"name": "healthtrace"}, {"name": "adisa"}],
+         "https://api.github.com/orgs/test")
+    ])
+    @unittest.mock.patch('client.get_json')
+    def test_public_repos(self, mock_payload, mock_url, mock_get):
+        '''
+        unit-test GithubOrgClient.public_repos
+        '''
+        mock_get.return_value = mock_payload
+        with unittest.mock.patch(
+                'client.GithubOrgClient._public_repos_url',
+                new_callable=PropertyMock) as mock_repo_url:
+            mock_repo_url.return_value = mock_url
+            test_client = GithubOrgClient("google")
+            self.assertEqual(
+                test_client.public_repos(),
+                [i['name'] for i in mock_payload])
+            mock_repo_url.assert_called_once()
+        mock_get.assert_called_once()
